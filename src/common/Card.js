c0cd3c559c2d113ad9f53b7eb01287653293271e
den777kaz <52310435+den777kaz@users.noreply.github.com>
2020-09-26 20:02:00 +0200

    tooltip

--- src/common/Card.js
+++ src/common/Card.js
@@ -10,13 +10,15 @@ const Card = (props) => {
     const imageUrl = "https://image.tmdb.org/t/p/w300";
     return (
 
-        <div className={style.card + " " + "card__maxW" }>
+        <div className={style.card + " " + "card__maxW"}>
             <div className={style.item}>
                 <div className={style.image}>
                     {
                         props.image
                             ? <img src={imageUrl + props.image} alt={props.title}/>
-                            : <img className={style.noImage} src={"https://www.ormistonhospital.co.nz/wp-content/uploads/2016/05/No-Image.jpg"} alt={props.title}/>
+                            : <img className={style.noImage}
+                                   src={"https://www.ormistonhospital.co.nz/wp-content/uploads/2016/05/No-Image.jpg"}
+                                   alt={props.title}/>
                     }
                     <div className={style.item__title}>
                         <h3>{props.title}</h3>
@@ -25,21 +27,23 @@ const Card = (props) => {
                 <div className={style.info}>
 
                     <div className={style.options}>
-
                         <span className={style.option + " " + style.play}>
                             <img src={playBlackIcon} alt=""/>
                         </span>
-                            <span className={style.option}>
+                        <span className={style.option}>
+                            <span className={style.tooltip}>Add to My List</span>
                             <img src={plusIcon} alt=""/>
                         </span>
-                            <span className={style.option}>
+                        <span className={style.option}>
+                          <span className={style.tooltip}>I Like This</span>
                             <img src={likeIcon} alt=""/>
                         </span>
-                            <span className={style.option}>
+                        <span className={style.option}>
+                            <span className={style.tooltip}>Not For Me</span>
                             <img src={dislikeIcon} alt=""/>
                         </span>
-
                         <span className={style.option}>
+                            <span className={style.tooltip}>More Info</span>
                         <img src={arrowDown} alt=""/>
                     </span>
                     </div>
